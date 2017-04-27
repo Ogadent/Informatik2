ecaae012b362bbdfd08dbd3f08c20aee520fed89
Ogadent <OgadentCoC@gmail.com>
2017-04-27 15:00:00 +0200

    Create Aufgabe2c.java

---
+++ blatt1/aufgabe2/Aufgabe2c.java
@@ -0,0 +1,10 @@
+package blatt1.aufgabe2;
+
+import java.util.Arrays;
+
+public class Aufgabe2c {
+
+	public static void main(String[] args) {
+		Arrays.stream(args).forEach(System.out::println);
+	}
+}
