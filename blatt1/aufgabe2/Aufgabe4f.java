b0cc1937620a901eeabd63f59eff0cfb30595801
Ogadent <OgadentCoC@gmail.com>
2017-04-27 15:01:03 +0200

    Create Aufgabe4f.java

---
+++ blatt1/aufgabe2/Aufgabe4f.java
@@ -0,0 +1,10 @@
+package blatt1.aufgabe2;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Aufgabe2f {
+
+	public static void main(String[] args) {
+		System.out.println(ThreadLocalRandom.current().nextDouble(0, 1));
+	}
+}
