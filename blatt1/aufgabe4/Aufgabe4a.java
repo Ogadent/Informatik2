d719db7c8b7b28541d2d99076a552c380726bf01
Ogadent <OgadentCoC@gmail.com>
2017-04-27 15:03:31 +0200

    Create Aufgabe4a.java

---
+++ blatt1/aufgabe4/Aufgabe4a.java
@@ -0,0 +1,24 @@
+package blatt1.aufgabe4;
+
+public class Aufgabe4a {
+
+	private static final int e = 2;
+	
+	public static void main(String[] args) {
+		System.out.println("Ergebnis: " + fakulty(e));
+	}
+	
+	public static int fakulty(final int num) {
+		if (num <= 2 && num >= 0) {
+			return num;
+		}
+		if (num < 0) {
+			throw new IllegalArgumentException("num must be >= 0");
+		}
+		int result = 6;
+		for (int i = 4; i <= num; i++) {
+			result *= i;
+		}
+		return result;
+	}
+}
