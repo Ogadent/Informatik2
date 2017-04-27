0828b897f83240ebea675f2d118db38e47951a57
Ogadent <OgadentCoC@gmail.com>
2017-04-27 15:01:56 +0200

    Create Aufgabe3a.java

---
+++ blatt1/aufgabe3/Aufgabe3a.java
@@ -0,0 +1,12 @@
+package blatt1.aufgabe3;
+
+public class Aufgabe3a {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Fehler");
+			return;
+		}
+		System.out.println(args[0].charAt(0));
+	}
+}
