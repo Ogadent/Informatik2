6accaeb00e26d0c3198e073b0f54f5478747acc5
Ogadent <OgadentCoC@gmail.com>
2017-05-01 23:32:49 +0200

    Update Aufgabe2c.java

--- blatt1/aufgabe2/Aufgabe2c.java
+++ blatt1/aufgabe2/Aufgabe2c.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class Aufgabe2c {
 
 	public static void main(String[] args) {
-		Arrays.stream(args).forEach(System.out::println);
+		System.out.println(args.length);
 	}
 }
