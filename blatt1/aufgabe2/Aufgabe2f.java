c7b04f6e4088ad23e32e4e823bccb13ad04225a1
Ogadent <OgadentCoC@gmail.com>
2017-05-02 23:17:42 +0200

    Update Aufgabe2f.java

--- blatt1/aufgabe2/Aufgabe2f.java
+++ blatt1/aufgabe2/Aufgabe2f.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Aufgabe2f {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		System.out.println(ThreadLocalRandom.current().nextDouble(0, 1));
 	}
 }
