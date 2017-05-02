f61f136b2dd1b4e53e84b757c466f1b3f435b88e
Ogadent <OgadentCoC@gmail.com>
2017-05-02 23:18:51 +0200

    Update Aufgabe3d.java

--- blatt1/aufgabe3/Aufgabe3d.java
+++ blatt1/aufgabe3/Aufgabe3d.java
@@ -2,7 +2,7 @@ package blatt1.aufgabe3;
 
 public class Aufgabe3d {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		if (args.length != 1) {
 			System.err.println("Fehler");
 			return;
