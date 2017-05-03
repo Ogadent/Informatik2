7962bf167a9eabdb1da55db140e0fcdee19c5a80
Ogadent <OgadentCoC@gmail.com>
2017-05-03 19:52:39 +0200

    Update Aufgabe3b.java

--- src/main/java/blatt1/aufgabe3/Aufgabe3b.java
+++ src/main/java/blatt1/aufgabe3/Aufgabe3b.java
@@ -2,7 +2,7 @@ package blatt1.aufgabe3;
 
 public class Aufgabe3b {
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		if (args.length != 2) {
 			System.err.println("Fehler");
 			return;
