956b61771e5cc6fe62c83009e2f021bfeb549d93
Ogadent <OgadentCoC@gmail.com>
2017-05-03 19:50:38 +0200

    Update Aufgabe4b.java

--- src/main/java/blatt1/aufgabe4/Aufgabe4b.java
+++ src/main/java/blatt1/aufgabe4/Aufgabe4b.java
@@ -4,7 +4,7 @@ public class Aufgabe4b {
 
 	private static final int NUM = 99;
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		System.out.println("Ergebnis: " + binaryCount(NUM));
 	}
 
