815623e426ae0aaa5f60d064dc3c1cb248e6fe8b
Ogadent <OgadentCoC@gmail.com>
2017-05-03 19:52:13 +0200

    Update Aufgabe2g.java

--- src/main/java/blatt1/aufgabe2/Aufgabe2g.java
+++ src/main/java/blatt1/aufgabe2/Aufgabe2g.java
@@ -2,7 +2,7 @@ package blatt1.aufgabe2;
 
 public class Aufgabe2g {
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		System.out.println(Math.sin(1.5));
 	}
 }
