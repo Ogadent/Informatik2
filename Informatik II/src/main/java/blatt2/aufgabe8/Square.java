b9a69343b535cf9b49881a1b7ac4e650a2786777
Ogadent <OgadentCoC@gmail.com>
2017-05-10 18:21:50 +0200

    Update Square.java

--- Informatik II/src/main/java/blatt2/aufgabe8/Square.java
+++ Informatik II/src/main/java/blatt2/aufgabe8/Square.java
@@ -35,42 +35,4 @@ public class Square {
 	public boolean checkPositive(final double num) {
 		return num > 0;
 	}
-
-	@Override
-	public String toString() {
-		return "Square [height=" + height + ", width=" + width + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(height);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(width);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Square other = (Square) obj;
-		if (Double.doubleToLongBits(height) != Double.doubleToLongBits(other.height)) {
-			return false;
-		}
-		if (Double.doubleToLongBits(width) != Double.doubleToLongBits(other.width)) {
-			return false;
-		}
-		return true;
-	}
 }
