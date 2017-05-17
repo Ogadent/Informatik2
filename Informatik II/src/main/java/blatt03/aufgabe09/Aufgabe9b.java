19a9e3557975ecd79eb1a6ff07fde8326dea5ae3
Ogadent <OgadentCoC@gmail.com>
2017-05-17 21:19:07 +0200

    Update Aufgabe9b.java

--- Informatik II/src/main/java/blatt03/aufgabe09/Aufgabe9b.java
+++ Informatik II/src/main/java/blatt03/aufgabe09/Aufgabe9b.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Aufgabe9b {
 
 	public static void main(String[] args) {
-		args = new String[]{"Max", "01.02.2100", "0112", null};
 		if (args.length < 1) {
 			return;
 		}
@@ -15,7 +14,6 @@ public class Aufgabe9b {
 		}
 		contact.setBirthday(args[1]);
 		Arrays.stream(args).forEach(contact::addTelephone);
-//		add if check?
 		contact.deleteTelephone("0112");
 		contact.getTelephone().forEach(System.out::println);
 	}
