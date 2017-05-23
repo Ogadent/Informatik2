6d264b1a257311d663f87e760b4e4d837732bdc5
Ogadent <OgadentCoC@gmail.com>
2017-05-24 00:24:26 +0200

    Update Student.java

--- Informatik II/src/main/java/blatt04/aufgabe15c/Student.java
+++ Informatik II/src/main/java/blatt04/aufgabe15c/Student.java
@@ -2,6 +2,6 @@ package blatt04.aufgabe15c;
 
 public class Student {
 
-    private List<Studiengang> studiengang = new ArrayList<>();
+    private List<Studium> studiengang = new ArrayList<>();
   
 }
